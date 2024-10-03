e48807702940e646f68b2cb36594ccecf4e35d0a
melle <melleming2001@gmail.com>
2024-10-03 16:36:24 +0200

    added feature to migrate config

---
+++ src/main/java/de/ftscraft/ftskampf/db/ConfigManager.java
@@ -0,0 +1,131 @@
+package de.ftscraft.ftskampf.db;
+
+import de.ftscraft.ftskampf.main.FTSKampf;
+import org.bukkit.configuration.file.FileConfiguration;
+import java.util.HashMap;
+
+public class ConfigManager {
+    private FTSKampf plugin;
+    private FileConfiguration config;
+
+    public ConfigManager() {
+        this.plugin = FTSKampf.getPlugin();
+        config = plugin.getConfig();
+        checkConfig();
+    }
+
+    private void checkConfig() {
+        HashMap<String, Object> defaultValues = new HashMap<>();
+        defaultValues.put("Testmode", false);
+        defaultValues.put("Permissions.UseDices.Name", "ftskampf.useDices");
+        defaultValues.put("Permissions.UseDices.Required", false);
+        defaultValues.put("Permissions.SetSkills.Name", "ftskampf.setSkills");
+        defaultValues.put("Permissions.SetSkills.Required", false);
+        defaultValues.put("Permissions.ResetSkills.Name", "ftskampf.resetSkills");
+        defaultValues.put("Permissions.ResetSkills.Required", false);
+        defaultValues.put("Permissions.ResetOtherSkills.Name", "ftskampf.resetOtherSkills");
+        defaultValues.put("Permissions.ResetOtherSkills.Required", false);
+        defaultValues.put("Permissions.Heal.Name", "ftskampf.heal");
+        defaultValues.put("Permissions.Heal.Required", false);
+        defaultValues.put("Permissions.DBAdmin.Name", "ftskampf.dbadmin");
+        defaultValues.put("Permissions.DBAdmin.Required", true);
+
+        defaultValues.put("Permissions.ChooseSpells.Name", "ftskampf.chooseSpells");
+        defaultValues.put("Permissions.ChooseSpells.Required", false);
+        defaultValues.put("Permissions.ResetSpells.Name", "ftskampf.resetSpells");
+        defaultValues.put("Permissions.ResetSpells.Required", false);
+        defaultValues.put("Permissions.ResetSpellsOther.Name", "ftskampf.resetOtherSpells");
+        defaultValues.put("Permissions.ResetSpellsOther.Required", false);
+
+        defaultValues.put("MaxSpells", 4);
+        defaultValues.put("EffectLifetime", 30);
+
+        defaultValues.put("DiceChatRange", 20);
+
+        defaultValues.put("Dice.Melee.Max", 100);
+        defaultValues.put("Dice.Melee.MaxSkillable", 100);
+        defaultValues.put("Dice.Distance.Max", 100);
+        defaultValues.put("Dice.Distance.MaxSkillable", 100);
+        defaultValues.put("Dice.Magic.Max", 100);
+        defaultValues.put("Dice.Magic.MaxSkillable", 100);
+        defaultValues.put("Dice.Agility.Max", 100);
+        defaultValues.put("Dice.Agility.MaxSkillable", 50);
+        defaultValues.put("Dice.Action.Max", 100);
+
+        defaultValues.put("Health.RegenRate", 30);
+        defaultValues.put("Health.RegenPoints", 1);
+        defaultValues.put("Health.SkillMultiplier", 5);
+        defaultValues.put("MinimumDamage", 5);
+
+        defaultValues.put("Armor.Leather", 20);
+        defaultValues.put("Armor.Gold", 25);
+        defaultValues.put("Armor.Chain", 25);
+        defaultValues.put("Armor.Iron", 35);
+        defaultValues.put("Armor.Diamond", 55);
+        defaultValues.put("Armor.Netherite", 70);
+
+        defaultValues.put("Armor.Shield", 8);
+
+        defaultValues.put("Weapon.Melee.Wood", 10);
+        defaultValues.put("Weapon.Melee.Stone", 15);
+        defaultValues.put("Weapon.Melee.Gold", 20);
+        defaultValues.put("Weapon.Melee.Iron", 35);
+        defaultValues.put("Weapon.Melee.Diamond", 50);
+        defaultValues.put("Weapon.Melee.Netherite", 65);
+        defaultValues.put("Weapon.Melee.Trident", 40);
+
+        defaultValues.put("Weapon.Melee.AxeDamageGain", 5);
+        defaultValues.put("Weapon.Melee.AxePrecisionLoss", 10);
+
+        defaultValues.put("Weapon.Distance.Bow", 30);
+        defaultValues.put("Weapon.Distance.Crossbow", 45);
+        defaultValues.put("Weapon.Distance.Trident", 40);
+
+        defaultValues.put("Weapon.Distance.CrossbowPrecisionLoss", 10);
+
+        defaultValues.put("Races.Default", "Mensch");
+        defaultValues.put("Races.List", new String[]{"Mensch", "Elf", "Zwerg", "Ork"});
+
+        defaultValues.put("Races.Mensch.MName", "Mensch");
+        defaultValues.put("Races.Mensch.FName", "Menschenfrau");
+        defaultValues.put("Races.Mensch.points", 100);
+        defaultValues.put("Races.Mensch.InitialValues.Melee", 10);
+        defaultValues.put("Races.Mensch.InitialValues.Distance", 15);
+        defaultValues.put("Races.Mensch.InitialValues.Magic", -1);
+        defaultValues.put("Races.Mensch.InitialValues.Agility", 8);
+        defaultValues.put("Races.Mensch.InitialValues.Health", 100);
+
+        defaultValues.put("Races.Elf.MName", "Elf");
+        defaultValues.put("Races.Elf.FName", "Elfe");
+        defaultValues.put("Races.Elf.points", 120);
+        defaultValues.put("Races.Elf.InitialValues.Melee", 8);
+        defaultValues.put("Races.Elf.InitialValues.Distance", 10);
+        defaultValues.put("Races.Elf.InitialValues.Magic", 15);
+        defaultValues.put("Races.Elf.InitialValues.Agility", 9);
+        defaultValues.put("Races.Elf.InitialValues.Health", 80);
+
+        defaultValues.put("Races.Zwerg.MName", "Zwerg");
+        defaultValues.put("Races.Zwerg.FName", "Zwergin");
+        defaultValues.put("Races.Zwerg.points", 120);
+        defaultValues.put("Races.Zwerg.InitialValues.Melee", 12);
+        defaultValues.put("Races.Zwerg.InitialValues.Distance", 12);
+        defaultValues.put("Races.Zwerg.InitialValues.Magic", 8);
+        defaultValues.put("Races.Zwerg.InitialValues.Agility", 7);
+        defaultValues.put("Races.Zwerg.InitialValues.Health", 120);
+
+        defaultValues.put("Races.Ork.MName", "Ork");
+        defaultValues.put("Races.Ork.FName", "Orkin");
+        defaultValues.put("Races.Ork.points", 120);
+        defaultValues.put("Races.Ork.InitialValues.Melee", 18);
+        defaultValues.put("Races.Ork.InitialValues.Distance",5);
+        defaultValues.put("Races.Ork.InitialValues.Magic", 5);
+        defaultValues.put("Races.Ork.InitialValues.Agility", 6);
+        defaultValues.put("Races.Ork.InitialValues.Health", 150);
+
+        for(String key : defaultValues.keySet()) {
+            if(!config.isSet(key)) config.set(key, defaultValues.get(key));
+        }
+
+        plugin.saveConfig();
+    }
+}
--- src/main/java/de/ftscraft/ftskampf/main/FTSKampf.java
+++ src/main/java/de/ftscraft/ftskampf/main/FTSKampf.java
@@ -3,10 +3,7 @@ package de.ftscraft.ftskampf.main;
 import de.ftscraft.ftsengine.main.Engine;
 import de.ftscraft.ftskampf.commands.*;
 import de.ftscraft.ftskampf.damageCalculators.DiceManager;
-import de.ftscraft.ftskampf.db.DBManager;
-import de.ftscraft.ftskampf.db.EffectManager;
-import de.ftscraft.ftskampf.db.HpManager;
-import de.ftscraft.ftskampf.db.SpellManager;
+import de.ftscraft.ftskampf.db.*;
 import de.ftscraft.ftskampf.listeners.InventoryListener;
 import de.ftscraft.ftskampf.listeners.JoinListener;
 import de.ftscraft.ftskampf.utils.HealthRunner;
@@ -29,6 +26,7 @@ import java.util.List;
 public final class FTSKampf extends JavaPlugin {
 
     private static FTSKampf plugin;
+    private static ConfigManager configManager;
     private static DiceManager diceManager;
     public static List<Inventory> kampfInventorys;
     public static List<MappedInventory> spellChooseInventory;//contains inventory and zid mapping
@@ -45,13 +43,12 @@ public final class FTSKampf extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+        configManager = new ConfigManager();
         kampfInventorys = new ArrayList<>();
         spellChooseInventory = new ArrayList<>();
         spellCastInventory = new ArrayList<>();
         spellTargetInventory = new HashMap<>();
         targetInventorys = new HashMap<>();
-
-        if (!doConfigExist()) createInitialConfig();
         raceList = getRaceList();
 
         engine = (Engine) getServer().getPluginManager().getPlugin("FTSEngine");
@@ -142,20 +139,6 @@ public final class FTSKampf extends JavaPlugin {
         return null;
     }
 
-    private boolean doConfigExist() {
-        File file = new File(getPath() + "/" + "config.yml");
-        return file.exists();
-    }
-
-    private File getPath() {
-        String path = System.getProperty("user.dir");
-        File directory = new File(path + "/plugins/" + plugin.getName());
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-        return directory;
-    }
-
     private List<String> getRaceList() {
         List<String> raceList = new ArrayList<>();
         FileConfiguration config = plugin.getConfig();
@@ -163,117 +146,5 @@ public final class FTSKampf extends JavaPlugin {
             raceList.add(race);
         }
         return raceList;
-    }
-
-    private void createInitialConfig() {
-        FileConfiguration config = plugin.getConfig();
-
-        config.set("Testmode", false);
-
-        config.set("Permissions.UseDices.Name", "ftskampf.useDices");
-        config.set("Permissions.UseDices.Required", false);
-        config.set("Permissions.SetSkills.Name", "ftskampf.setSkills");
-        config.set("Permissions.SetSkills.Required", false);
-        config.set("Permissions.ResetSkills.Name", "ftskampf.resetSkills");
-        config.set("Permissions.ResetSkills.Required", false);
-        config.set("Permissions.ResetOtherSkills.Name", "ftskampf.resetOtherSkills");
-        config.set("Permissions.ResetOtherSkills.Required", false);
-        config.set("Permissions.Heal.Name", "ftskampf.heal");
-        config.set("Permissions.Heal.Required", false);
-        config.set("Permissions.DBAdmin.Name", "ftskampf.dbadmin");
-        config.set("Permissions.DBAdmin.Required", true);
-
-        config.set("Permissions.ChooseSpells.Name", "ftskampf.chooseSpells");
-        config.set("Permissions.ChooseSpells.Required", false);
-        config.set("Permissions.ResetSpells.Name", "ftskampf.resetSpells");
-        config.set("Permissions.ResetSpells.Required", false);
-        config.set("Permissions.ResetSpellsOther.Name", "ftskampf.resetOtherSpells");
-        config.set("Permissions.ResetSpellsOther.Required", false);
-
-        config.set("MaxSpells", 4);
-        config.set("EffectLifetime", 30);
-
-        config.set("DiceChatRange", 20);
-
-        config.set("Dice.Melee.Max", 100);
-        config.set("Dice.Melee.MaxSkillable", 100);
-        config.set("Dice.Distance.Max", 100);
-        config.set("Dice.Distance.MaxSkillable", 100);
-        config.set("Dice.Magic.Max", 100);
-        config.set("Dice.Magic.MaxSkillable", 100);
-        config.set("Dice.Agility.Max", 100);
-        config.set("Dice.Agility.MaxSkillable", 50);
-        config.set("Dice.Action.Max", 100);
-
-        config.set("Health.RegenRate", 30);
-        config.set("Health.RegenPoints", 1);
-        config.set("Health.SkillMultiplier", 5);
-        config.set("MinimumDamage", 5);
-
-        config.set("Armor.Leather", 20);
-        config.set("Armor.Gold", 25);
-        config.set("Armor.Chain", 25);
-        config.set("Armor.Iron", 35);
-        config.set("Armor.Diamond", 55);
-        config.set("Armor.Netherite", 70);
-
-        config.set("Armor.Shield", 8);
-
-        config.set("Weapon.Melee.Wood", 10);
-        config.set("Weapon.Melee.Stone", 15);
-        config.set("Weapon.Melee.Gold", 20);
-        config.set("Weapon.Melee.Iron", 35);
-        config.set("Weapon.Melee.Diamond", 50);
-        config.set("Weapon.Melee.Netherite", 65);
-        config.set("Weapon.Melee.Trident", 40);
-
-        config.set("Weapon.Melee.AxeDamageGain", 5);
-        config.set("Weapon.Melee.AxePrecisionLoss", 10);
-
-        config.set("Weapon.Distance.Bow", 30);
-        config.set("Weapon.Distance.Crossbow", 45);
-        config.set("Weapon.Distance.Trident", 40);
-
-        config.set("Weapon.Distance.CrossbowPrecisionLoss", 10);
-
-        config.set("Races.Default", "Mensch");
-        config.set("Races.List", new String[]{"Mensch", "Elf", "Zwerg", "Ork"});
-
-        config.set("Races.Mensch.MName", "Mensch");
-        config.set("Races.Mensch.FName", "Menschenfrau");
-        config.set("Races.Mensch.points", 100);
-        config.set("Races.Mensch.InitialValues.Melee", 10);
-        config.set("Races.Mensch.InitialValues.Distance", 15);
-        config.set("Races.Mensch.InitialValues.Magic", -1);
-        config.set("Races.Mensch.InitialValues.Agility", 8);
-        config.set("Races.Mensch.InitialValues.Health", 100);
-
-        config.set("Races.Elf.MName", "Elf");
-        config.set("Races.Elf.FName", "Elfe");
-        config.set("Races.Elf.points", 120);
-        config.set("Races.Elf.InitialValues.Melee", 8);
-        config.set("Races.Elf.InitialValues.Distance", 10);
-        config.set("Races.Elf.InitialValues.Magic", 15);
-        config.set("Races.Elf.InitialValues.Agility", 9);
-        config.set("Races.Elf.InitialValues.Health", 80);
-
-        config.set("Races.Zwerg.MName", "Zwerg");
-        config.set("Races.Zwerg.FName", "Zwergin");
-        config.set("Races.Zwerg.points", 120);
-        config.set("Races.Zwerg.InitialValues.Melee", 12);
-        config.set("Races.Zwerg.InitialValues.Distance", 12);
-        config.set("Races.Zwerg.InitialValues.Magic", 8);
-        config.set("Races.Zwerg.InitialValues.Agility", 7);
-        config.set("Races.Zwerg.InitialValues.Health", 120);
-
-        config.set("Races.Ork.MName", "Ork");
-        config.set("Races.Ork.FName", "Orkin");
-        config.set("Races.Ork.points", 120);
-        config.set("Races.Ork.InitialValues.Melee", 18);
-        config.set("Races.Ork.InitialValues.Distance",5);
-        config.set("Races.Ork.InitialValues.Magic", 5);
-        config.set("Races.Ork.InitialValues.Agility", 6);
-        config.set("Races.Ork.InitialValues.Health", 150);
-        plugin.saveConfig();
     }
 }
